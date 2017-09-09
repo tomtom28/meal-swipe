70266bfbba28764af566336f50aa54476957bfc4
Tommy Thompson <tomthompson828@gmail.com>
2017-09-09 00:52:06 -0400

    toggling footer, should be changed to tabs not action

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Container } from 'native-base';
+import { Router, Scene } from 'react-native-router-flux';
 
 // Custom Component Imports
 import AppFooter from './app/components/AppFooter';
@@ -13,27 +14,46 @@ import ViewSwipe from './app/components/ViewSwipe';
 export default class App extends Component {
   render() {
     return (
-      <Container>
 
+      <Container>
 
         <AppHeader />
 
 
-
-        <Container>
-          {/* <ViewCuisine /> */}
-          {/* <ViewAbout /> */}
-          <ViewSwipe />
-          {/* <ViewSelect /> */}
-        </Container>
-
-
+        <Router>
+          <Scene key="root">
+            <Scene key="about"
+              component={ViewAbout}
+              title="About"
+              tabs={true}
+              initial
+            />
+            <Scene
+              key="cuisine"
+              component={ViewCuisine}
+              title="Cuisine"
+              tabs={true}
+            />
+            <Scene
+              key="swipe"
+              component={ViewSwipe}
+              title="Swipe"
+              tabs={true}
+            />
+            <Scene
+              key="select"
+              component={ViewSelect}
+              title="Select"
+              tabs={true}
+            />
+          </Scene>
+        </Router>
 
 
         <AppFooter />
 
-
       </Container>
+
     );
   }
 }
--- app/components/AppFooter.js
+++ app/components/AppFooter.js
@@ -1,27 +1,33 @@
 import React, { Component } from 'react';
 import { Button, Footer, FooterTab, Icon, Text } from 'native-base';
+import { Actions } from 'react-native-router-flux'; 
 
 export default class AppFooter extends Component {
   render() {
     return (
       <Footer>
         <FooterTab>
-          <Button vertical active>
-            <Icon active name="person" />
+
+          <Button onPress={() => Actions.about()} >
+            <Icon name="person" />
             <Text>About</Text>
           </Button>
-          <Button vertical>
+
+          <Button onPress={() => Actions.cuisine()} >
             <Icon name="pizza" />
             <Text>Cuisine</Text>
           </Button>
-          <Button vertical>
+
+          <Button onPress={() => Actions.swipe()} >
             <Icon name="speedometer" />
             <Text>Swipe</Text>
           </Button>
-          <Button vertical>
+
+          <Button onPress={() => Actions.select()} >
             <Icon name="bulb" />
             <Text>Select</Text>
           </Button>
+
         </FooterTab>
       </Footer>
     );
